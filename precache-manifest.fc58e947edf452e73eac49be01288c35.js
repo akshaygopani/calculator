88876d80408fe6ad5406ea6ce376418aa946a337
root <akshaygopani10@gmail.com>
2020-02-09 16:03:03 +0530

    Updates

---
+++ precache-manifest.fc58e947edf452e73eac49be01288c35.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fd8524e32a2e1327fe7525d0eaba7f3",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "76c9cafb380ad8a70845",
+    "url": "/calculator/static/js/2.13efc7e7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/calculator/static/js/2.13efc7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1f04f1521e3d9ba54c0",
+    "url": "/calculator/static/js/main.4253bef1.chunk.js"
+  },
+  {
+    "revision": "28828353795638a97109",
+    "url": "/calculator/static/js/runtime-main.63ccf6e2.js"
+  }
+]);
